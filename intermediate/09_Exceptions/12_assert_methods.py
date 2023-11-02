c7f9e007a7ea504eb44103eb6ef15b2f00093605
luisAlcides <luisalcidesblandon@gmail.com>
2023-11-01 20:36:30 -0600

    Day 23

---
+++ intermediate/09_Exceptions/12_assert_methods.py
@@ -0,0 +1,16 @@
+# Assert Methods
+
+import unittest
+import alerts
+
+
+class SystemAlertTests(unittest.TestCase):
+
+    def test_power_outage_alert(self):
+        self.assertRaises(alerts.PowerError, alerts.power_outage_detected, True)
+
+    def test_water_levels_warnings(self):
+        self.assertWarns(alerts.WaterLevelWarning, alerts.water_levels_check, 150)
+
+
+unittest.main()
