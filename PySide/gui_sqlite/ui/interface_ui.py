9a5a3c05a79c16ea4fa4015d44278a56244a0dac
heracle <heracle@pop-os.localdomain>
2024-02-05 23:50:11 -0600

    Day 35

---
+++ PySide/gui_sqlite/ui/interface_ui.py
@@ -0,0 +1,306 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'interface.ui'
+##
+## Created by: Qt User Interface Compiler version 6.6.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QFrame, QHBoxLayout, QLabel,
+    QMainWindow, QPushButton, QSizePolicy, QSpacerItem,
+    QStackedWidget, QVBoxLayout, QWidget)
+
+from Custom_Widgets.Widgets import QCustomSlideMenu
+import _icons_rc
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(930, 582)
+        MainWindow.setStyleSheet(u"*{\n"
+"	border: none;\n"
+"	background-color: transparent;\n"
+"	background: transparent;\n"
+"	padding: 0;\n"
+"	margin: 0;\n"
+"	color: #fff\n"
+"}\n"
+"\n"
+"#centralwidget, #homeBtn, #mainBodyContent{\n"
+"	background-color: #4a4a5c;\n"
+"}\n"
+"\n"
+"#header, #mainbody{\n"
+"	background-color: #5c5c70;\n"
+"}\n"
+"\n"
+"\n"
+"\n"
+"#btnuser{\n"
+"	border: 1px solid #cc5bc3;\n"
+"	border-radius: 19px;\n"
+"	text-align: center;\n"
+"}\n"
+"\n"
+"\n"
+"QPushButton{\n"
+"	text-align: left;\n"
+"	padding: 5px 10px;\n"
+"	border-top-left-radius: 10px;\n"
+"	border-bottom-left-radius: 10px;\n"
+"	\n"
+"\n"
+"}")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.centralwidget.setStyleSheet(u"")
+        self.verticalLayout = QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSpacing(10)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.header = QWidget(self.centralwidget)
+        self.header.setObjectName(u"header")
+        self.horizontalLayout = QHBoxLayout(self.header)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.frame_2 = QFrame(self.header)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_4 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_4.setObjectName(u"horizontalLayout_4")
+        self.menuBtn = QPushButton(self.frame_2)
+        self.menuBtn.setObjectName(u"menuBtn")
+        icon = QIcon()
+        icon.addFile(u":/feather/Icons/feather/align-justify.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.menuBtn.setIcon(icon)
+        self.menuBtn.setIconSize(QSize(24, 24))
+
+        self.horizontalLayout_4.addWidget(self.menuBtn, 0, Qt.AlignLeft)
+
+        self.label = QLabel(self.frame_2)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setPointSize(12)
+        font.setBold(True)
+        self.label.setFont(font)
+
+        self.horizontalLayout_4.addWidget(self.label)
+
+
+        self.horizontalLayout.addWidget(self.frame_2, 0, Qt.AlignLeft)
+
+        self.frame = QFrame(self.header)
+        self.frame.setObjectName(u"frame")
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.horizontalLayout_3 = QHBoxLayout(self.frame)
+        self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
+        self.pushButton = QPushButton(self.frame)
+        self.pushButton.setObjectName(u"pushButton")
+        icon1 = QIcon()
+        icon1.addFile(u":/feather/Icons/feather/search.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton.setIcon(icon1)
+        self.pushButton.setIconSize(QSize(16, 16))
+
+        self.horizontalLayout_3.addWidget(self.pushButton)
+
+        self.pushButton_3 = QPushButton(self.frame)
+        self.pushButton_3.setObjectName(u"pushButton_3")
+        icon2 = QIcon()
+        icon2.addFile(u":/feather/Icons/feather/bell.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_3.setIcon(icon2)
+        self.pushButton_3.setIconSize(QSize(16, 16))
+
+        self.horizontalLayout_3.addWidget(self.pushButton_3)
+
+        self.pushButton_2 = QPushButton(self.frame)
+        self.pushButton_2.setObjectName(u"pushButton_2")
+        icon3 = QIcon()
+        icon3.addFile(u":/feather/Icons/feather/activity.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButton_2.setIcon(icon3)
+        self.pushButton_2.setIconSize(QSize(16, 16))
+
+        self.horizontalLayout_3.addWidget(self.pushButton_2)
+
+        self.btnuser = QPushButton(self.frame)
+        self.btnuser.setObjectName(u"btnuser")
+        self.btnuser.setMinimumSize(QSize(38, 38))
+        self.btnuser.setMaximumSize(QSize(38, 38))
+        self.btnuser.setStyleSheet(u"#btnuser{\n"
+"	color: white;\n"
+"}")
+        icon4 = QIcon()
+        icon4.addFile(u":/feather/Icons/feather/user.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.btnuser.setIcon(icon4)
+        self.btnuser.setIconSize(QSize(32, 32))
+
+        self.horizontalLayout_3.addWidget(self.btnuser)
+
+
+        self.horizontalLayout.addWidget(self.frame, 0, Qt.AlignRight)
+
+
+        self.verticalLayout.addWidget(self.header, 0, Qt.AlignTop)
+
+        self.mainbody = QWidget(self.centralwidget)
+        self.mainbody.setObjectName(u"mainbody")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.mainbody.sizePolicy().hasHeightForWidth())
+        self.mainbody.setSizePolicy(sizePolicy)
+        self.horizontalLayout_2 = QHBoxLayout(self.mainbody)
+        self.horizontalLayout_2.setSpacing(0)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, -1, 0, 0)
+        self.leftMenu = QCustomSlideMenu(self.mainbody)
+        self.leftMenu.setObjectName(u"leftMenu")
+        self.leftMenu.setMinimumSize(QSize(300, 0))
+        self.verticalLayout_3 = QVBoxLayout(self.leftMenu)
+        self.verticalLayout_3.setSpacing(0)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.widget = QWidget(self.leftMenu)
+        self.widget.setObjectName(u"widget")
+        self.verticalLayout_4 = QVBoxLayout(self.widget)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
+        self.verticalLayout_4.setContentsMargins(0, 0, 0, 0)
+        self.frame_3 = QFrame(self.widget)
+        self.frame_3.setObjectName(u"frame_3")
+        self.frame_3.setFrameShape(QFrame.StyledPanel)
+        self.frame_3.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_5 = QVBoxLayout(self.frame_3)
+        self.verticalLayout_5.setSpacing(10)
+        self.verticalLayout_5.setObjectName(u"verticalLayout_5")
+        self.verticalLayout_5.setContentsMargins(0, 0, 0, 0)
+        self.homeBtn = QPushButton(self.frame_3)
+        self.homeBtn.setObjectName(u"homeBtn")
+        icon5 = QIcon()
+        icon5.addFile(u":/feather/Icons/feather/home.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.homeBtn.setIcon(icon5)
+
+        self.verticalLayout_5.addWidget(self.homeBtn)
+
+        self.reportsBtn = QPushButton(self.frame_3)
+        self.reportsBtn.setObjectName(u"reportsBtn")
+        icon6 = QIcon()
+        icon6.addFile(u":/material_design/Icons/material_design/report.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.reportsBtn.setIcon(icon6)
+
+        self.verticalLayout_5.addWidget(self.reportsBtn)
+
+        self.accountBtn = QPushButton(self.frame_3)
+        self.accountBtn.setObjectName(u"accountBtn")
+        icon7 = QIcon()
+        icon7.addFile(u":/material_design/Icons/material_design/account_circle.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.accountBtn.setIcon(icon7)
+
+        self.verticalLayout_5.addWidget(self.accountBtn)
+
+
+        self.verticalLayout_4.addWidget(self.frame_3)
+
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout_4.addItem(self.verticalSpacer)
+
+        self.frame_4 = QFrame(self.widget)
+        self.frame_4.setObjectName(u"frame_4")
+        self.frame_4.setFrameShape(QFrame.StyledPanel)
+        self.frame_4.setFrameShadow(QFrame.Raised)
+        self.verticalLayout_6 = QVBoxLayout(self.frame_4)
+        self.verticalLayout_6.setSpacing(10)
+        self.verticalLayout_6.setObjectName(u"verticalLayout_6")
+        self.verticalLayout_6.setContentsMargins(0, 0, 0, 0)
+        self.settingsBtn = QPushButton(self.frame_4)
+        self.settingsBtn.setObjectName(u"settingsBtn")
+        icon8 = QIcon()
+        icon8.addFile(u":/feather/Icons/feather/settings.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.settingsBtn.setIcon(icon8)
+
+        self.verticalLayout_6.addWidget(self.settingsBtn)
+
+        self.helpBtn = QPushButton(self.frame_4)
+        self.helpBtn.setObjectName(u"helpBtn")
+        icon9 = QIcon()
+        icon9.addFile(u":/feather/Icons/feather/help-circle.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.helpBtn.setIcon(icon9)
+
+        self.verticalLayout_6.addWidget(self.helpBtn)
+
+        self.aboutBtn = QPushButton(self.frame_4)
+        self.aboutBtn.setObjectName(u"aboutBtn")
+        icon10 = QIcon()
+        icon10.addFile(u":/material_design/Icons/material_design/roundabout_left.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.aboutBtn.setIcon(icon10)
+
+        self.verticalLayout_6.addWidget(self.aboutBtn)
+
+
+        self.verticalLayout_4.addWidget(self.frame_4)
+
+
+        self.verticalLayout_3.addWidget(self.widget)
+
+
+        self.horizontalLayout_2.addWidget(self.leftMenu)
+
+        self.mainBodyContent = QWidget(self.mainbody)
+        self.mainBodyContent.setObjectName(u"mainBodyContent")
+        self.verticalLayout_2 = QVBoxLayout(self.mainBodyContent)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.stackedWidget = QStackedWidget(self.mainBodyContent)
+        self.stackedWidget.setObjectName(u"stackedWidget")
+        self.page = QWidget()
+        self.page.setObjectName(u"page")
+        self.stackedWidget.addWidget(self.page)
+        self.page_2 = QWidget()
+        self.page_2.setObjectName(u"page_2")
+        self.stackedWidget.addWidget(self.page_2)
+
+        self.verticalLayout_2.addWidget(self.stackedWidget)
+
+
+        self.horizontalLayout_2.addWidget(self.mainBodyContent)
+
+        self.rightMenu = QWidget(self.mainbody)
+        self.rightMenu.setObjectName(u"rightMenu")
+
+        self.horizontalLayout_2.addWidget(self.rightMenu)
+
+
+        self.verticalLayout.addWidget(self.mainbody)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.menuBtn.setText("")
+        self.label.setText(QCoreApplication.translate("MainWindow", u"My SQLITE App", None))
+        self.pushButton.setText("")
+        self.pushButton_3.setText("")
+        self.pushButton_2.setText("")
+        self.btnuser.setText("")
+        self.homeBtn.setText(QCoreApplication.translate("MainWindow", u"Home", None))
+        self.reportsBtn.setText(QCoreApplication.translate("MainWindow", u"Reports", None))
+        self.accountBtn.setText(QCoreApplication.translate("MainWindow", u"My Account", None))
+        self.settingsBtn.setText(QCoreApplication.translate("MainWindow", u"Settings", None))
+        self.helpBtn.setText(QCoreApplication.translate("MainWindow", u"Help", None))
+        self.aboutBtn.setText(QCoreApplication.translate("MainWindow", u"About", None))
+    # retranslateUi
+
