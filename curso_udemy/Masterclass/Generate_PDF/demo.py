5c6d551e1707e1c1b182f5fa35c02fa44e4ebade
luisAlcides <luisalcidesblandon@gmail.com>
2024-03-14 22:41:56 -0600

    Day 45

---
+++ curso_udemy/Masterclass/Generate_PDF/demo.py
@@ -0,0 +1,22 @@
+from fpdf import FPDF
+
+
+class PDF(FPDF):
+    def header(self):
+        self.image('logo.png', 10, 8, 33)
+        self.set_font('helvetica', 'B', 16)
+        self.cell(80)
+        self.cell(40,10, 'Title', border=1, align='C')
+        self.ln(40)
+
+    def footer(self):
+        self.set_y(-15)
+        self.set_font('helvetica', 'I', 8)
+        self.cell(0, 10, f'Page {self.page_no()}/{{nb}}', align='C')
+
+pdf = PDF()
+pdf.add_page()
+pdf.set_font('helvetica', size=12)
+for i in range(1,41):
+    pdf.cell(0, 10, f'Printing line number: {i}', new_x='LMARGIN', new_y='NEXT')
+pdf.output('sample.pdf')
